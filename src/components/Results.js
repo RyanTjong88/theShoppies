36291cddaf136c3b5b82a995e3248b83272e0c86
RyanTjong88 <ryan.tjong888@gmail.com>
2021-01-15 01:37:57 -0500

    made a successful API request / rendered title, year, and nominate button to the DOM

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const Results = (props) => {
-    console.log(props.title)
 
+    // const test = [...props.titles, ...props.releaseDates]
+    // console.log(test)
+
+    // const title = props.titles.map((ran) => {
+    //     // return <p>{ran}</p>
+    //     return <div>
+    //                 <p>{ran}</p>
+    //                 {/* <p>{releaseDate}</p> */}
+    //                 <button>nom</button>
+    //             </div>
+    // });
+    const test = props.res.map(results => {
+        const test2 = results.Title
+        const test3 = results.Year
+
+        
+        return  <div>
+                    <p>{test2} ({test3})</p>
+                    <button>Nominate</button>
+                </div>
+    })
+    
+    console.log(test)
+        
+    //  console.log(test)
     return (
-        <div>
-            <h2>{props.title}{props.releaseDate}</h2>
-        </div>
+        <>
+           {test}
+        </>
     );
 };
 
